c3a0cf258c8c5fd46debddc350a24921ba73a18c
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-10 13:46:55 +0530

    added email send functionality

--- src/main/java/com/neec/email/service/EmailSender.java
+++ src/main/java/com/neec/email/service/EmailSender.java
@@ -3,13 +3,39 @@ package com.neec.email.service;
 import java.util.Map;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;;
 
 @Service
 public class EmailSender {
+	@Value("${spring.mail.username}")
+	private String fromEmailAddress;
+
+	@Value("${account.verification.url}")
+	private String accountVerificationUrl;
+
+	private JavaMailSender javaMailSender;
+
+	public EmailSender(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
+	}
 
 	@RabbitListener(queues = {"${rabbitmq.email.queue.name}"})
 	public void sendMessage(Map<String, String> message) {
-		System.out.println("📩 Received message: " + message);
+		String recipientEmail = message.get("email");
+		String token = message.get("token");
+		String completeAccountVerificationUrl = accountVerificationUrl.concat(token);
+		String emailContent = new StringBuffer("Thank you for registering for the National Entrance Exam for Commerce.")
+				.append("\n\nPlease click the link below to verify your account:\n")
+				.append(completeAccountVerificationUrl).toString();
+
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setFrom(fromEmailAddress);
+		email.setTo(recipientEmail);
+		email.setSubject("NEEC Account Verification");
+		email.setText(emailContent);
+		javaMailSender.send(email);
     }
 }
